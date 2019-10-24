069333e5039d42738c4170fe4cb056253c1fb00b
dendickys <febri.unquad2014@gmail.com>
2019-10-24 22:20:32 +0700

    fix WebView

--- app/src/main/java/id/dendickys/mywebviewexpert/MainActivity.java
+++ app/src/main/java/id/dendickys/mywebviewexpert/MainActivity.java
@@ -1,8 +1,13 @@
 package id.dendickys.mywebviewexpert;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.webView);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                view.loadUrl("javascript:alert('Web fgo-minsk berhasil dimuat')");
+            }
+        });
+
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                result.confirm();
+                return true;
+            }
+        });
+
+        myWebView.loadUrl("https://www.fgo-minsk.id");
     }
 }
